88c4cce4d57f330923f27f7fc80de179e483e0a6
Tamasco <tamasco_tau@hotmail.com>
2016-11-29 18:06:43 +0100

    Javascript

---
+++ js/min.js
@@ -0,0 +1,16 @@
+
+
+$.noConflict();
+jQuery(document).ready(function ($) {
+    var valor = $("#busqueda").val();
+    console.log(valor);
+
+    valor = $("#busqueda").attr("value");
+    console.log(valor);
+
+    /*
+     var nombre = "Borja";
+     console.log("Hola " + nombre)
+     */
+
+});
