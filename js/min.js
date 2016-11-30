1742a7050f5ac116c46d7a0cf76d4239aa3f886f
Tamasco <tamasco_tau@hotmail.com>
2016-11-30 18:03:36 +0100

    DNI

--- js/min.js
+++ js/min.js
@@ -1,16 +1,39 @@
 
-
 $.noConflict();
 jQuery(document).ready(function ($) {
-    var valor = $("#busqueda").val();
-    console.log(valor);
 
-    valor = $("#busqueda").attr("value");
-    console.log(valor);
+    function tracear() {
+        var valor;
 
-    /*
-     var nombre = "Borja";
-     console.log("Hola " + nombre)
-     */
+        // RECOGE EL VALOR DEL ATRIBUTO
+        valor = $("#busqueda").val();
+        console.log(valor);
 
+        // ESTABLECE UN VALOR
+        $("#busqueda").val("Borja");
+        valor = $("#busqueda").attr("value");
+        console.log(valor);
+    }
+
+    //tracear();
+    //var valor;
+    //valor = $("#busqueda").val();
+    //calcularDNI(valor)
+
+    $("#main a.btn").click(function(){
+        var dni = $("#busqueda").val();
+        var letra = calcularDNI(dni);
+        console.log(letra);
+        $("#main span.resultado").text("Resultado: " + letra);
+        //e.preventDefault();
+        return false;
+    });
 });
+
+function calcularDNI(dni){
+    cadena="TRWAGMYFPDXBNJZSQVHLCKET";
+    posicion = dni % 23;
+    letra = cadena.substring(posicion,posicion+1);
+    //alert("Letra DNI: " + letra);
+    return letra;
+}
